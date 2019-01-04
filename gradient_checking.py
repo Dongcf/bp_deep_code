82820f477f94eb00f56e839aab86e9951ca1cf2c
Dongcf <41134756+Dongcf@users.noreply.github.com>
2019-01-04 09:53:52 +0800

    Delete gradient_checking.py

--- gradient_checking.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-class GradientCheck(object):
-	def gradient_check(self,x,theta,epsilon=1e-7):
-		thetaplus = theta + epsilon
-		thrtaminus = theta - epsilon
-		J_plus = forward_propagation(x,thetaplus)
